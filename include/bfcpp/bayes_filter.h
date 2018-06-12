18a6699f5fbac2f531127d2a417ea7f0caaad4c8
Fabian Meyer <user8324@posteo.net>
2018-06-12 09:55:05 +0200

    Add default construtor for bayes filter

--- include/bfcpp/bayes_filter.h
+++ include/bfcpp/bayes_filter.h
@@ -8,7 +8,6 @@
 #ifndef BFCPP_BAYES_FILTER_H_
 #define BFCPP_BAYES_FILTER_H_
 
-#include <cassert>
 #include "bfcpp/models.h"
 
 namespace bf
@@ -19,7 +18,37 @@ namespace bf
         MotionModel *motionModel_;
         SensorModel *sensorModel_;
 
-    protected:
+    public:
+
+        BayesFilter()
+            : motionModel_(nullptr), sensorModel_(nullptr)
+        {
+
+        }
+
+        BayesFilter(MotionModel *mm, SensorModel *sm)
+            : motionModel_(mm), sensorModel_(sm)
+        {
+        }
+
+        virtual ~BayesFilter()
+        {
+            if(sensorModel_ != nullptr)
+                delete sensorModel_;
+            if(motionModel_ != nullptr)
+                delete motionModel_;
+        }
+
+        void setMotionModel(MotionModel *mm)
+        {
+            motionModel_ = mm;
+        }
+
+        void setSensorModel(SensorModel *sm)
+        {
+            sensorModel_ = sm;
+        }
+
         MotionModel &motionModel()
         {
             return *motionModel_;
@@ -40,33 +69,37 @@ namespace bf
             return *sensorModel_;
         }
 
-    public:
-
-        BayesFilter(MotionModel *mm, SensorModel *sm)
-            : motionModel_(mm), sensorModel_(sm)
-        {
-            assert(motionModel_ != nullptr);
-            assert(sensorModel_ != nullptr);
-        }
-
-        virtual ~BayesFilter()
-        {
-            assert(motionModel_ != nullptr);
-            assert(sensorModel_ != nullptr);
-
-            delete motionModel_;
-            delete sensorModel_;
-        }
-
+        /** Return the current estimated state vector of the filter and its
+         *  covariance.
+         *  @return pair of state vector and covariance */
         virtual std::pair<Eigen::VectorXd, Eigen::MatrixXd> getEstimate() const = 0;
 
+        /** Initialize the filter with the given state vector and covariance.
+         *  @param state initial state vector of size Nx1
+         *  @param cov initial covariance of size NxN*/
         virtual void init(const Eigen::VectorXd &state,
                           const Eigen::MatrixXd &cov) = 0;
+
+        /** Execute prediction step of the bayes filter with the motion model.
+         *  @param controls control vector
+         *  @param observations observation matrix, each column is one observation
+         *  @param noise noise matrix of the motion model of size NxN */
         virtual void predict(const Eigen::VectorXd &controls,
                              const Eigen::MatrixXd &observations,
-                             const Eigen::MatrixXd &motionCov) = 0;
+                             const Eigen::MatrixXd &noise) = 0;
+
+        /** Execute correction step of the bayes filter with the sensor model.
+         *  @param observations observation matrix, each column is one observation of the length M
+         *  @param noise noise matrix of the sensor model of size MxM */
         virtual void correct(const Eigen::MatrixXd &observations,
-                             const Eigen::MatrixXd &sensorCov) = 0;
+                             const Eigen::MatrixXd &noise) = 0;
+
+        /** Update the filter by one discrete timestep, which runs prediction
+         *  and correction step.
+         *  @param controls control vector
+         *  @param observations observation matrix, each column is one observation of the length M
+         *  @param motionCov noise matrix of the motion model of size NxN
+         *  @param sensorCov noise matrix of the sensor model of size MxM */
         void update(const Eigen::VectorXd &controls,
                     const Eigen::MatrixXd &observations,
                     const Eigen::MatrixXd &motionCov,
